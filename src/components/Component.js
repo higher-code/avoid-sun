7fc2e2bd74b4d23941985393c34c3381d3dae55f
Taesu Hyeon <qvil1127@gmail.com>
2018-03-24 23:20:27 +0900

    Initialize by Create React Native App
    * .babelrc : module-resolver root => src

---
+++ src/components/Component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+class Component extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Text>Component</Text>
+    );
+  }
+}
+
+export default Component;
